fff875ca6c385aae33a0923504f69f3376dff533
bartlomiej <bartlomiejmazur24@gmail.com>
2022-11-20 09:28:35 +0100

    Download API key and code in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Configuration, OpenAIApi} from 'openai'
 
 import './App.css';
 
 function App() {
+  
+
+    const configuration = new Configuration({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+
+    const openai = new OpenAIApi(configuration);
+
+    console.log(openai);
+    
+
   return (
     <div className="App">
    
