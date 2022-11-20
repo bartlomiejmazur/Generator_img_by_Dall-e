d1347be3f24e6ba190afe9494998c1633a8bbb05
bartlomiej <bartlomiejmazur24@gmail.com>
2022-11-20 11:42:42 +0100

    Connect key with API and create fun getImage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import React, { useState } from 'react';
-import { Configuration, OpenAIApi} from 'openai'
 
+import { Configuration, OpenAIApi} from 'openai';
 import './App.css';
 
+
+
 function App() {
+
+  const configuration = new Configuration({
+    apiKey: process.env.REACT_APP_KEY,
+  });
+ 
   
-
-    const configuration = new Configuration({
-      apiKey: process.env.OPENAI_API_KEY,
-    });
-
+  const [prompt, setPrompt] = useState('')
     const openai = new OpenAIApi(configuration);
+    const getImage = async () => {
+    const response = await openai.createImage({
+     prompt: "Wolf on fire",
+     n: 1,
+     size: "1024x1024",
+     })
+     console.log(response.data.data[0].url);
+    };
 
-    console.log(openai);
+    
+     
     
 
   return (
-    <div className="App">
-   
+    <div className="app-main">
+    <h1>Generate your own image by Artificial intelligence (Dalle-E) </h1>
+    <input className='app-input' type="text" />
+      <button onClick={getImage}>Generate image</button>
     </div>
   );
 }
